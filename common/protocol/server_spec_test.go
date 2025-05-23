20f72b127d534e4f4b7d970c4d4f1030926498f3
domaingts <139693268+domaingts@users.noreply.github.com>
2025-05-23 13:01:02 +0000

    remove removed references

--- common/protocol/server_spec_test.go
+++ common/protocol/server_spec_test.go
@@ -5,10 +5,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/xtls/xray-core/common"
 	"github.com/xtls/xray-core/common/net"
 	. "github.com/xtls/xray-core/common/protocol"
-	"github.com/xtls/xray-core/common/uuid"
 )
 
 func TestAlwaysValidStrategy(t *testing.T) {
